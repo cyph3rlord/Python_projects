000367b2c60d0b3d42522e15cc83080bf4de4f44
cyph3rlord <samuelaondoaverterngu@gmail.com>
2026-08-27 11:29:00 +0000

    Added a function

--- beginner_projects/alice.py
+++ beginner_projects/alice.py
@@ -1,12 +1,24 @@
+def count_words(file_name):
+    """ Count the approximat number of words in a file"""
+    try:
+        with open(file_name) as file_object:
+            contents = file_object.read()
+    except FileNotFoundError:
+        print(f"The file name {file_name}, does not exist.")
+    else:
+        words = contents.split()
+        print(f"The file {file_name}, has {len(words)} words.")
 
-file_name = "alice.txt"
-try:
- 
-  with open(file_name) as file_object:
-     contents = file_object.read()
 
-except FileNotFoundError:
-    print(f"The file name {file_nmae}, does not exist.")
-else:
-    words = contents.split()
-    print(len(f"The book Alice in Wonderland has {words} words."))
+
+file_names = ["alice.txt", "Siddhartha.txt", "mobydick.txt", "littlewoman.txt"]
+
+for file_name in file_names:
+    count_words(file_name)
+
+
+
+
+
+
+
